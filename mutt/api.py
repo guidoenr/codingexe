789c24131e6588886b7ffb3a765beda4aaa6ef8e
guido.enrique <guido.enrique@avature.net>
2022-03-11 11:59:57 -0300

    quick commit

---
+++ mutt/api.py
@@ -0,0 +1,26 @@
+# @author: Guido Enrique
+# @github: /guidoenr
+# @gitlab: /guidoenr
+
+import requests
+import json
+import logger
+
+
+COINGECKO_API = 'https://api.coingecko.com/api/v3'
+
+def get_request(subdomain:str, *params):
+    url = COINGECKO_API + subdomain
+    logger.log(f'GET to {subdomain}, params={params}')
+    return requests.get(url, *params).json()
+
+if __name__ == '__main__':
+    #print(get_request("/ping"))
+    #print(get_request("/coins/list"))
+    #print(get_request("/coins/categories/list"))
+    payload = {
+        'deveoper_data': 'true', 
+        'market_data': 'true',
+        'page': 1
+    }
+    get_request("/coins/solana/")
