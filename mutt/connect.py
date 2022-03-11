ff7301b1be8071d42ae78d261a330f382a4bbad4
guido.enrique <guido.enrique@avature.net>
2022-03-11 16:31:17 -0300

    quick commit

---
+++ mutt/connect.py
@@ -0,0 +1,28 @@
+from configparser import ConfigParser
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+import logger
+
+def config(filename='database.ini', section='postgresql'):
+    parser = ConfigParser()
+    parser.read(filename)
+
+    db = {}
+    if parser.has_section(section):
+        params = parser.items(section)
+        for param in params:
+            db[param[0]] = param[1]
+    else:
+        raise Exception('Section {0} not found in the {1} file'.format(section, filename))
+
+    return db
+
+def get_engine():
+    params = config()
+    db_uri = 'postgresql+psycopg2://{user}:{password}@{host}/{database}'.format(**params)
+    engine = create_engine(db_uri, echo=True)
+    return engine
+
+def get_session():
+    Session = sessionmaker(get_engine())
+    return Session()
