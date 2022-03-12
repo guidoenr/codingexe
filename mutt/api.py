6ab517ddee41f8eeadc61a694ff2a1e69acc796b
guido.enrique <guido.enrique@avature.net>
2022-03-12 15:49:19 -0300

    quick commit

--- mutt/api.py
+++ mutt/api.py
@@ -4,20 +4,25 @@
 
 import requests
 import logger
+import json
 
 COINGECKO_API = 'https://api.coingecko.com/api/v3'
 
-def get_request(subdomain:str, *params):
+"""
+payload = {
+    'deveoper_data': 'true', 
+    'market_data': 'true',
+    'page': 1
+}
+"""
+def get_data(subdomain:str, *params): # params could be optional
     url = COINGECKO_API + subdomain
-    return requests.get(url, *params).json()
+    return requests.get(url, *params).json() # it is str
 
 if __name__ == '__main__':
     #print(get_request("/ping"))
     #print(get_request("/coins/list"))
     #print(get_request("/coins/categories/list"))
-    payload = {
-        'deveoper_data': 'true', 
-        'market_data': 'true',
-        'page': 1
-    }
-    get_request("/coins/solana/")
+    data = get_data("/coins/bitcoin/")
+    
+    
